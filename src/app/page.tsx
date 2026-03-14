96593dc3bbd6ca6b9c7b2e1d5127aba69e8751e6
GeoLise <jora.liseenko@yandex.ru>
2026-03-14 15:20:01 +0300

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/dialog";
 import { Image } from "@/components/ui/image";
 import { api } from "@/server/api";
+import { getAuthServerSession } from "@/server/auth";
 import { MessageSquareCheck } from "lucide-react";
 import NextImage from "next/image";
 
@@ -17,6 +18,10 @@ export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const products = (await api.products.get()).data;
+
+  const session = await getAuthServerSession();
+
+  console.log(session);
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-800 font-sans dark:bg-black">
@@ -33,23 +38,6 @@ export default async function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-accent-foreground">
             To get started, edit the page.tsx file.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
